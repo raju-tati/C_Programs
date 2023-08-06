4e00f8ef140bb725ab1d867916fe0c9abbd1400d
raju-tati <118584250+raju-tati@users.noreply.github.com>
2023-08-06 15:41:55 +0530

    Update simpleTree.c

--- simpleTree.c
+++ simpleTree.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct treeNode
-{
+struct treeNode {
 	int data;
 	struct treeNode *leftNode;
 	struct treeNode *rightNode;
@@ -10,31 +9,17 @@ struct treeNode
 
 static struct treeNode treeNodes[7];
 
-struct treeNode*
-tSearch (struct treeNode* root, int v)
-{
-	while (root)
-	{
-		printf
-		(
-			"looking for %d, looking at %d",
-			v,
-			root->data
-		);
-		
-		if (root->data == v)
-		{
+struct treeNode* tSearch (struct treeNode* root, int v) {
+	while (root) {
+		printf ( "looking for %d, looking at %d", v, root->data );
+		if (root->data == v) {
 			return(root);
 		}
-		
-		if (root->data > v)
-		{
+	
+		if (root->data > v) {
 			printf(" at left node of the tree\n");
 			root = root->leftNode;
-		}
-		
-		else
-		{
+		} else {
 			printf(" at right node of the tree\n");
 			root = root->rightNode;
 		}
@@ -43,20 +28,16 @@ tSearch (struct treeNode* root, int v)
 	return(0);
 }
 
-int
-main(void)
-{
+int main(void) {
 	struct treeNode *temporaryNode, *rootNode;
 	int counter;
 	
-	for(counter = 0; counter < 7; counter++)
-	{
+	for(counter = 0; counter < 7; counter++) {
 		int increment;
 		increment = counter + 1;
 		
 		treeNodes[counter].data = increment;
-		if (increment == 2 || increment == 6)
-		{
+		if (increment == 2 || increment == 6) {
 			treeNodes[counter].leftNode  = &treeNodes[counter - 1];
 			treeNodes[counter].rightNode = &treeNodes[counter + 1];
 		}
@@ -67,16 +48,13 @@ main(void)
 	rootNode->rightNode = &treeNodes[5];
 	
 	temporaryNode = tSearch(rootNode, 9);
-	if(temporaryNode)
-	{
+	if(temporaryNode) {
 		printf("found at position %d\n", temporaryNode);
 	}
 	
-	else
-	{
+	else {
 		printf("value not found\n");
 	}
 	
 	exit(EXIT_SUCCESS);
 }
-
